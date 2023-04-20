a3f4a652f70102d9c441650ee71413d9dd42ac38
Carlos Henrique Silva Nascimento <carlosminibics@gmail.com>
2023-04-20 19:07:53 -0300

    Add files via upload

---
+++ concatenação.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Aula PHP</title>
+    </head>
+
+    <body>
+
+        <?php
+
+        $nome = 'carlos';
+        $cor = 'azul com preto ';
+        $anos= '22 anos';
+
+        //operador .
+        
+            echo 'Olá '.$nome.', vi que sua cor preferida é '.$cor.', estou vendo também que você possui '.$anos.' e que gostar de jogar videogame';
+
+            //aspas duplas
+            echo '<br />';
+            echo '<br />';
+            echo '<br />';
+           
+            echo "ola $nome";
+        ?>
+    </body>
+</html>
