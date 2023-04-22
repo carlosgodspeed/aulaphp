8d9eb3cdcfbc4a1f0bfa55cf6eaeecafff68b790
Carlos Henrique Silva Nascimento <carlosminibics@gmail.com>
2023-04-22 20:11:12 -0300

    Add files via upload

---
+++ if-else_operadores-condicionais.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Aula PHP</title>
+    </head>
+
+    <body>
+
+        <?php
+              
+             // == igual
+
+             // === identico
+
+             // != ou <> diferente
+
+             // !== não identico
+
+             // < menor
+
+             // > maior
+
+             // <= menor igual
+
+             // >= maior igual
+
+            if(25>=30) {
+               echo 'verdadeiro';
+            }else {
+               echo 'falso';
+            }
+            
+        ?>
+
+    </body>
+</html>
