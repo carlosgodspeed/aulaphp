06314009b5c5a8c70f6264714940abdd9e2df9e6
Carlos Henrique Silva Nascimento <carlosminibics@gmail.com>
2023-04-25 15:31:14 -0300

    Add files via upload

---
+++ if-else.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Aula PHP</title>
+    </head>
+
+    <body>
+
+        <?php
+            
+            $usuario_possui_cartao_loja = true;
+            $valor_compra = 225;
+
+            $valor_frete = 50;
+            $recebeu_desconto_frete = false;
+
+            if($usuario_possui_cartao_loja == true && $valor_compra >= 100) {
+                $valor_frete = 0;
+                $recebeu_desconto_frete = true;
+            }
+        ?>
+
+        <h1>Detalhes do pedido</h1>
+
+        <p>possui cartão da loja?
+            <?php
+               if($usuario_possui_cartao_loja == true) {
+                   echo 'SIM';
+                }else {
+                   echo 'NÃO';
+                } 
+            ?>
+        </p>
+
+        <p>Valor da compra: <?= $valor_compra ?> </p>
+
+        <p>Recebeu desconto no frete?
+            <?php
+               if($recebeu_desconto_frete == true) {
+                   echo 'SIM';
+                }else {
+                   echo 'NÃO';
+                } 
+            ?>
+        </p>
+
+        <p>Valor do frete: <? $valor_frete ?></p>
+
+    </body>
+</html>
