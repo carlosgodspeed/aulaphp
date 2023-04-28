eb9abffa132f1dd8c447b73b218b8123f103b4ad
Carlos Henrique Silva Nascimento <carlosminibics@gmail.com>
2023-04-28 15:12:31 -0300

    Add files via upload

---
+++ operadores_atribuicao_aritmeticos.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>aula php</title>
+    </head>
+
+    <body>
+
+        <?php
+
+             $x = 10;
+             $y = 8;
+
+             $x += $y;
+
+            echo $x;
+
+        ?>
+
+    </body>
+</html>
