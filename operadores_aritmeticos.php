c6a9e8f85054db5838c6c3a6c4568520ed896d1a
Carlos Henrique Silva Nascimento <carlosminibics@gmail.com>
2023-04-28 15:01:08 -0300

    Add files via upload

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>aula php</title>
+    </head>
+
+    <body>
+
+        <?php
+
+             $num1 = 13;
+             $num2 = 4;
+
+             echo "A soma entre o $num1 e $num2 é " . ($num1 + $num2);
+             echo '<br/>';
+             echo "A subtração entre o $num1 e $num2 é " . ($num1 - $num2);
+             echo '<br/>';
+             echo "A multiplicação entre o $num1 e $num2 é " . ($num1 * $num2);
+             echo '<br/>';
+             echo "A divisão entre o $num1 e $num2 é " . ($num1 / $num2);
+             echo '<br/>';
+             echo "O módulo entre o $num1 e $num2 é " . ($num1 % $num2);
+        ?>
+
+    </body>
+</html>
