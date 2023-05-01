3abb35552b79b1d7dc7e5ceeed99d51c849bd5a2
Carlos Henrique Silva Nascimento <carlosminibics@gmail.com>
2023-04-30 22:29:43 -0300

    Add files via upload

---
+++ funcoes_matematicas.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>aula php</title>
+    </head>
+
+     <body>
+
+            <?php
+
+                 //ceil -> arredonda o valor para cima 
+                 //floor -> arredonda o valor para baixo
+                 //round -> arredonda o valor com base nas casas decimais
+                 //rand -> gera um inteiro aleatorio
+                 //sqrt -> retorna a raiz quadrada
+            
+                 $num = 7.3;
+                 echo ceil($num);
+
+                 echo '<br/>';
+
+                 $num = 7.3;
+                 echo floor($num);
+
+                 echo '<br/>';
+
+                 $num = 7.3;
+                 echo round($num);
+
+                 echo '<br/>';
+
+                 echo rand();
+
+                 echo '<br/>';
+
+                 echo sqrt(100);
+
+            ?>
+     </body>
+</html>
